c81154d2d0470e9a0ea6e2402d6692523cc08e64
Darren Ldl <darrenldldev@gmail.com>
2019-10-12 12:14:20 +1100

    Progress on lxc_c.ml

--- src/lxc_c.ml
+++ src/lxc_c.ml
@@ -50,6 +50,30 @@ module Bdev_specs = struct
   let rbd = field t "rbd" Rbd.t
 end
 
+module Migrate_opts = struct
+  type t
+
+  let t : t structure typ = structure "migrate_opts"
+
+  let directory = field t "directory" (ptr char)
+
+  let verbose = field t "verbose" bool
+
+  let stop = field t "stop" bool
+
+  let predump_dir = field t "predump_dir" (ptr char)
+
+  let pageserver_address = field t "pageserver_address" (ptr char)
+
+  let pageserver_port = field t "pageserver_port" (ptr char)
+
+  let preserves_inodes = field t "preserves_inodes" bool
+
+  let action_script = field t "action_script" (ptr char)
+
+  let disable_skip_in_flight = field t "disable_skip_in_flight"
+end
+
 type lxc_container
 
 let lxc_container : lxc_container structure typ = structure "lxc_container"
@@ -128,3 +152,128 @@ let create =
     (funptr
        ( ptr lxc_container @-> ptr char @-> ptr char @-> ptr Bdev_specs.t
          @-> int @-> ptr char @-> returning bool ))
+
+let rename =
+  field lxc_container "rename"
+    (funptr
+       (ptr lxc_container @-> ptr char @-> returning bool))
+
+let reboot =
+  field lxc_container "reboot"
+    (funptr (ptr lxc_container @-> returning bool))
+
+let shutdown =
+  field lxc_container "shutdown"
+    (funptr (ptr lxc_container @-> int @-> returning bool))
+
+let clear_config =
+  field lxc_container "clear_config"
+    (funptr (ptr lxc_container @-> returning void))
+
+let clear_config_item =
+  field lxc_container "clear_config_item"
+    (funptr (ptr lxc_container @-> ptr char @-> returning bool))
+
+let get_config_item =
+  field lxc_container "get_config_item"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> int @-> returning int))
+
+let get_running_config_item =
+  field lxc_container "get_running_config_item"
+    (funptr (ptr lxc_container @-> ptr char @-> returning (ptr char)))
+
+let get_keys =
+  field lxc_container "get_keys"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> int @-> returning int))
+
+let get_interfaces =
+  field lxc_container "get_interfaces"
+    (funptr (ptr lxc_container @-> returning (ptr (ptr char))))
+
+let get_ips =
+  field lxc_container "get_ips"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> int @-> returning (ptr (ptr char))))
+
+let get_cgroup_item =
+  field lxc_container "get_cgroup_item"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> int @-> returning int))
+
+let set_cgroup_item =
+  field lxc_container "set_cgroup_item"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> returning bool))
+
+let get_config_path =
+  field lxc_container "get_config_path"
+    (funptr (ptr lxc_container @-> returning (ptr char)))
+
+let set_config_path =
+  field lxc_container "set_config_path"
+    (funptr (ptr lxc_container @-> ptr char @-> returning bool))
+
+let clone =
+  field lxc_container "clone"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> ptr char @-> int @-> ptr char @-> ptr char @-> uint64_t @-> ptr (ptr char) @-> returning (ptr lxc_container)))
+
+let console_getfd =
+  field lxc_container "console_getfd"
+    (funptr (ptr lxc_container @-> ptr int @-> ptr int @-> returning int))
+
+let console =
+  field lxc_container "console"
+    (funptr (ptr lxc_container @-> int @-> int @-> int @-> int @-> int @-> returning int))
+
+let snapshot =
+  field lxc_container "snapshot"
+    (funptr (ptr lxc_container @-> ptr char @-> returning int))
+
+let snapshot_list =
+  field lxc_container "snapshot_list"
+    (funptr (ptr lxc_container @-> ptr (ptr lxc_container) @-> returning int))
+
+let snapshot_restore =
+  field lxc_container "snapshot_restore"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> returning bool))
+
+let snapshot_destroy =
+  field lxc_container "snapshot_destroy"
+    (funptr (ptr lxc_container @-> ptr char @-> returning bool))
+
+let may_control =
+  field lxc_container "may_control"
+    (funptr (ptr lxc_container @-> returning bool))
+
+let add_device_node =
+  field lxc_container "add_device_node"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> returning bool))
+
+let remove_device_node =
+  field lxc_container "remove_device_node"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> returning bool))
+
+let attach_interface =
+  field lxc_container "attach_interface"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> returning bool))
+
+let detach_interface =
+  field lxc_container "detach_interface"
+    (funptr (ptr lxc_container @-> ptr char @-> ptr char @-> returning bool))
+
+let checkpoint =
+  field lxc_container "checkpoint"
+    (funptr (ptr lxc_container @-> ptr char @-> bool @-> bool @-> returning bool))
+
+let restore =
+  field lxc_container "restore"
+    (funptr (ptr lxc_container @-> ptr char @-> bool @-> returning bool))
+
+let destroy_with_snapshots =
+  field lxc_container "destroy_with_snapshots"
+    (funptr (ptr lxc_container @-> returning bool))
+
+let snapshot_destroy_all =
+  field lxc_container "snapshot_destroy_all"
+    (funptr (ptr lxc_container @-> returning bool))
+
+let migrate =
+  field lxc_container "migrate"
+    (funptr (ptr lxc_container @-> uint @-> ))
