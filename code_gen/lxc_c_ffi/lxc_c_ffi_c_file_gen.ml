f2c2ff54387aa5b62b2e8e30cd45b71321e8152d
Darren Ldl <darrenldldev@gmail.com>
2019-10-12 18:38:15 +1100

    Moved glue code into separate file

--- code_gen/lxc_c_ffi/lxc_c_ffi_c_file_gen.ml
+++ code_gen/lxc_c_ffi/lxc_c_ffi_c_file_gen.ml
@@ -1,53 +1,16 @@
-let headers = ["#include <lxc/lxccontainer.h>";
-               "#include <stdint.h>"]
-
-let glue = {|
-struct bdev_specs_glue {
-	char *fstype;
-	uint64_t fssize;
-	struct zfs_glue {
-		char *zfsroot;
-	} zfs;
-	struct lvm_glue {
-		char *vg;
-		char *lv;
-		char *thinpool;
-	} lvm;
-	char *dir;
-	struct rbd_glue {
-		char *rbdname;
-		char *rbdpool;
-	} rbd;
-};
-
-struct bdev_specs bdev_specs_glue_dissolve(struct bdev_specs_glue * src) {
-  struct bdev_specs ret = { 0 };
-
-  ret.fstype = src->fstype;
-  ret.fssize = src->fssize;
-
-  ret.zfs.zfsroot = src->zfs.zfsroot;
-
-  ret.lvm.vg = src->lvm.vg;
-  ret.lvm.lv = src->lvm.lv;
-  ret.lvm.thinpool = src->lvm.thinpool;
-
-  ret.dir = src->dir;
-
-  ret.rbd.rbdname = src->rbd.rbdname;
-  ret.rbd.rbdpool = src->rbd.rbdpool;
-
-  return ret;
-}
-|}
-
 let () =
   let c_out = stdout in
   let c_fmt = Format.formatter_of_out_channel c_out in
-  List.iter (fun h ->
-      output_string c_out (Printf.sprintf "%s\n" h)
-    ) headers;
-  output_string c_out "\n";
-  output_string c_out glue;
-  output_string c_out "\n";
+  let c_in = open_in "lxc_glue.c" in
+  Fun.protect ~finally:(fun () -> close_in c_in)
+    (fun () ->
+       try
+         while true do
+           let line = input_line c_in in
+           output_string c_out line;
+           output_string c_out "\n";
+         done;
+       with
+       | End_of_file -> ()
+    );
   Cstubs_structs.write_c c_fmt (module Ffi_bindings.Bindings)
