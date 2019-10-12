88f076803c492e20b86803bb586aa19278d4ab6c
Darren Ldl <darrenldldev@gmail.com>
2019-10-12 16:42:28 +1100

    Progress on stub generation

--- code_gen/lxc_c_ffi/lxc_c_ffi_c_file_gen.ml
+++ code_gen/lxc_c_ffi/lxc_c_ffi_c_file_gen.ml
@@ -1,12 +1,9 @@
-let headers = ["#include <lxccontainer.h>"]
+let headers = ["#include <lxc/lxccontainer.h>"]
 
 let () =
-  let c_out = open_out "lxc_c_ffi_ml_file_gen.c" in
-  Fun.protect ~finally:(fun () -> close_out c_out)
-    (fun () ->
-       let c_fmt = Format.formatter_of_out_channel c_out in
-       List.iter (fun h ->
-           Format.fprintf c_fmt "%s\n" h
-         ) headers;
-       Cstubs_structs.write_c c_fmt (module Ffi_bindings.Bindings)
-    )
+  let c_out = stdout in
+  let c_fmt = Format.formatter_of_out_channel c_out in
+  List.iter (fun h ->
+      Format.fprintf c_fmt "%s\n" h
+    ) headers;
+  Cstubs_structs.write_c c_fmt (module Ffi_bindings.Bindings)
