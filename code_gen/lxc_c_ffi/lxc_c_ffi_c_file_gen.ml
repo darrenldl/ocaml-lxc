3edad7c4a5f67b39c1544733a3c65cf1a7a69e10
Darren Ldl <darrenldldev@gmail.com>
2019-10-12 17:03:08 +1100

    Moving back to dynamic FFI design

--- code_gen/lxc_c_ffi/lxc_c_ffi_c_file_gen.ml
+++
@@ -1,9 +0,0 @@
-let headers = ["#include <lxc/lxccontainer.h>"]
-
-let () =
-  let c_out = stdout in
-  let c_fmt = Format.formatter_of_out_channel c_out in
-  List.iter (fun h ->
-      Format.fprintf c_fmt "%s\n" h
-    ) headers;
-  Cstubs_structs.write_c c_fmt (module Ffi_bindings.Bindings)
