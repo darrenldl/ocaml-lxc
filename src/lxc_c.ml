448030aa587bcd4f79ca3079be59876e0c44983b
Darren Ldl <darrenldldev@gmail.com>
2019-10-12 19:18:33 +1100

    Progress on lxc_c

--- src/lxc_c.ml
+++ src/lxc_c.ml
@@ -1,1 +1,22 @@
-module Stubs = Ffi_bindings.Stubs(Ml_stubs)
+module Stubs = Ffi_bindings.Stubs (Ml_stubs)
+
+module Migrate_cmd = struct
+  open Stubs.Migrate_cmd
+
+  type t =
+    | Migrate_pre_dump
+    | Migrate_dump
+    | Migrate_restore
+    | Migrate_feature_check
+
+  let to_c_int t =
+    match t with
+    | Migrate_pre_dump ->
+      migrate_pre_dump
+    | Migrate_dump ->
+      migrate_dump
+    | Migrate_restore ->
+      migrate_restore
+    | Migrate_feature_check ->
+      migrate_feature_check
+end
