b8fd819d7ed888955eecce9ce57e35351df0ee72
Darren Ldl <darrenldldev@gmail.com>
2019-10-13 21:25:28 +1100

    Added function stubs generation

--- code_gen/stubs_gen.ml
+++ code_gen/stubs_gen.ml
@@ -13,19 +13,27 @@ let gen_ffi_types_stubs () =
                 let line = input_line c_in in
                 output_string c_out line; output_string c_out "\n"
               done
-            with End_of_file -> ();
-         );
-       Cstubs.Types.write_c c_fmt (module Ffi_types.Types_stubs)
-    )
+            with End_of_file -> ());
+       Cstubs.Types.write_c c_fmt (module Ffi_types.Types_stubs))
 
-let gen_ffi_binding_stubs =
-  let c_out = open_out "ffi_bindings.c" in
-  let c_fmt = Format.formatter_of_out_channel c_out in
-  let ml_out = open_out "ffi_bindings.ml" in
-  Fun.protect
-    ~finally:(fun () -> close_out c_out)
-    (fun () ->
-       Cstubs.write_c c_fmt ~prefix:"ffi_" (module Ffi_bindings.Fun_stubs)
-    )
+let gen_ffi_binding_stubs () =
+  begin
+    let c_out = open_out "ffi_bindings_c_stubs.c" in
+    let c_fmt = Format.formatter_of_out_channel c_out in
+    Fun.protect
+      ~finally:(fun () -> close_out c_out)
+      (fun () ->
+         Cstubs.write_c c_fmt ~prefix:"ffi_" (module Ffi_bindings.Fun_stubs)
+      );
+  end;
+  begin
+    let ml_out = open_out "ffi_bindings_ml_stubs.ml" in
+    let ml_fmt = Format.formatter_of_out_channel ml_out in
+    Fun.protect
+      ~finally:(fun () -> close_out ml_out)
+      (fun () ->
+         Cstubs.write_ml ml_fmt ~prefix:"ffi_" (module Ffi_bindings.Fun_stubs);
+      )
+  end
 
-let () = gen_ffi_types_stubs ()
+let () = gen_ffi_types_stubs (); gen_ffi_binding_stubs ()
--- src/lxc_c.ml
+++ src/lxc_c.ml
@@ -1,5 +1,8 @@
 open Ctypes
+
 module Type_stubs = Ffi_types.Types_stubs (Ffi_types_ml_stubs)
+
+module Fun_stubs = Ffi_bindings.Fun_stubs (Ffi_bindings_ml_stubs)
 
 exception Unexpected_value
 
