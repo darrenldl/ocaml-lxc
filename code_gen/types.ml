23640d39cb4ac8da9a27415cf83e35f48e1a7fa8
Darren Ldl <darrenldldev@gmail.com>
2019-10-13 18:51:05 +1100

    Formatting

--- code_gen/types.ml
+++ code_gen/types.ml
@@ -3,29 +3,25 @@ open Ctypes
 module Lxc_attach_options_t = struct
   type t
 
-  let t : t structure typ =
-    structure "lxc_attach_options_t"
+  let t : t structure typ = structure "lxc_attach_options_t"
 end
 
 module Lxc_snapshot = struct
   type t
 
-  let t : t structure typ =
-    structure "lxc_snapshot"
+  let t : t structure typ = structure "lxc_snapshot"
 end
 
 module Zfs_glue = struct
   type t
 
-  let t : t structure typ =
-    structure "zfs_glue"
+  let t : t structure typ = structure "zfs_glue"
 end
 
 module Bdev_specs_glue = struct
   type t
 
-  let t : t structure typ =
-    structure "bdev_specs_glue"
+  let t : t structure typ = structure "bdev_specs_glue"
 end
 
 type lxc_container
