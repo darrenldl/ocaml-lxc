71603f3bae60ef3e34c1fdf00839bd2880d880bf
Darren Ldl <darrenldldev@gmail.com>
2019-10-13 21:50:59 +1100

    Moved lxc_glue defs into separate header file

--- code_gen/lxc_glue.c
+++ code_gen/lxc_glue.c
@@ -1,24 +1,4 @@
-#include <lxc/lxccontainer.h>
-#include <lxc/attach_options.h>
-#include <stdint.h>
-
-struct bdev_specs_glue {
-  char *fstype;
-  uint64_t fssize;
-  struct zfs_glue {
-    char *zfsroot;
-  } zfs;
-  struct lvm_glue {
-    char *vg;
-    char *lv;
-    char *thinpool;
-  } lvm;
-  char *dir;
-  struct rbd_glue {
-    char *rbdname;
-    char *rbdpool;
-  } rbd;
-};
+#include "lxc_glue.h"
 
 struct bdev_specs bdev_specs_glue_dissolve(struct bdev_specs_glue * src) {
   struct bdev_specs ret = { 0 };
@@ -40,8 +20,8 @@ struct bdev_specs bdev_specs_glue_dissolve(struct bdev_specs_glue * src) {
   return ret;
 }
 
-bool create_glue(struct lxc_container *c, const char *t, const char *bdevtype,
-                 struct bdev_specs_glue *specs_glue, int flags, char *const argv[]) {
+bool create_glu(struct lxc_container *c, const char *t, const char *bdevtype,
+                struct bdev_specs_glue *specs_glue, int flags, char *const argv[]) {
   struct bdev_specs specs = bdev_specs_glue_dissolve(specs_glue);
 
   return c->create(c, t, bdevtype, &specs, flags, argv);
---
+++ code_gen/lxc_glue.h
@@ -0,0 +1,31 @@
+#include <lxc/lxccontainer.h>
+#include <lxc/attach_options.h>
+#include <stdint.h>
+
+#ifndef LXC_GLUE_H
+#define LXC_GLUE_H
+
+struct bdev_specs_glue {
+  char *fstype;
+  uint64_t fssize;
+  struct zfs_glue {
+    char *zfsroot;
+  } zfs;
+  struct lvm_glue {
+    char *vg;
+    char *lv;
+    char *thinpool;
+  } lvm;
+  char *dir;
+  struct rbd_glue {
+    char *rbdname;
+    char *rbdpool;
+  } rbd;
+};
+
+struct bdev_specs bdev_specs_glue_dissolve(struct bdev_specs_glue * src);
+
+bool create_glu(struct lxc_container *c, const char *t, const char *bdevtype,
+                struct bdev_specs_glue *specs_glue, int flags, char *const argv[]);
+
+#endif
