b16a33bc477156fe5ee6fcb8dfd6c358f20c11d4
Darren Ldl <darrenldldev@gmail.com>
2019-10-13 21:51:54 +1100

    Updated glue functions suffix

--- code_gen/ffi_bindings.ml
+++ code_gen/ffi_bindings.ml
@@ -4,8 +4,11 @@ open Types
 module Fun_stubs (F : Cstubs.FOREIGN) = struct
   open F
 
-  let create_glue =
-    foreign "create_glue"
+  (*$*)
+(*$*)
+
+  let create_glu =
+    foreign "create_glu"
       ( ptr lxc_container @-> string @-> string @-> ptr_opt Bdev_specs_glue.t
         @-> int @-> ptr_opt string @-> returning bool )
 end
