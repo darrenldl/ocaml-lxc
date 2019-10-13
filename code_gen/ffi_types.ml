b846a91ee3c6fc6c79dd2193538c21269dda6283
Darren Ldl <darrenldldev@gmail.com>
2019-10-13 22:42:56 +1100

    Code refactoring

--- code_gen/ffi_types.ml
+++ code_gen/ffi_types.ml
@@ -237,7 +237,7 @@ module Types_stubs (S : Cstubs_structs.TYPE) = struct
 
   (*$ #use "code_gen/gen.cinaps";;
 
-       gen_lxc_container_funptr_field_all ()
+       gen_lxc_container_funptr_field_ml_all ()
   *)
 
   let is_defined =
