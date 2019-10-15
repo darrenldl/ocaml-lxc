85ddc1ddee5bf9f143f4db7f9aacc9fb5d2c4401
Darren Ldl <darrenldldev@gmail.com>
2019-10-15 18:12:06 +1100

    Added list container names functions

--- src/lxc.ml
+++ src/lxc.ml
@@ -39,11 +39,37 @@ let get_global_config_item ~key =
 
 let get_version () = Lxc_c.lxc_get_version ()
 
-let list_defined_containers ~(lxcpath : string) =
+(*$
+     List.iter
+       (fun name ->
+          Printf.printf "let %s_names ~(lxcpath : string) =\n" name;
+          Printf.printf "  let name_arr_ptr_typ = ptr (ptr char) in\n";
+          Printf.printf
+            "  let name_arr_ptr_init = coerce (ptr void) name_arr_ptr_typ null in\n";
+          Printf.printf
+            "  let struct_arr_ptr_typ = ptr (ptr Types.lxc_container) in\n";
+          Printf.printf
+            "  let name_arr_ptr = allocate name_arr_ptr_typ name_arr_ptr_init in";
+          Printf.printf
+            "  let struct_arr_ptr_null = coerce (ptr void) (ptr \
+             struct_arr_ptr_typ) null in\n";
+          Printf.printf "  let count =\n";
+          Printf.printf
+            "    Lxc_c.%ss lxcpath name_arr_ptr struct_arr_ptr_null in\n" name;
+          Printf.printf
+            "  let name_ptr_list = CArray.from_ptr name_arr_ptr count |> \
+             CArray.to_list in\n";
+          Printf.printf
+            "  name_ptr_list |> List.map (fun ptr -> string_from_null_term_ptr \
+             !@ptr)\n")
+       ["list_defined_container"; "list_active_container"; "list_all_container"]
+*)
+
+let list_defined_container_names ~(lxcpath : string) =
   let name_arr_ptr_typ = ptr (ptr char) in
   let name_arr_ptr_init = coerce (ptr void) name_arr_ptr_typ null in
-  let name_arr_ptr = allocate name_arr_ptr_typ name_arr_ptr_init in
   let struct_arr_ptr_typ = ptr (ptr Types.lxc_container) in
+  let name_arr_ptr = allocate name_arr_ptr_typ name_arr_ptr_init in
   let struct_arr_ptr_null = coerce (ptr void) (ptr struct_arr_ptr_typ) null in
   let count =
     Lxc_c.list_defined_containers lxcpath name_arr_ptr struct_arr_ptr_null
@@ -51,4 +77,29 @@ let list_defined_containers ~(lxcpath : string) =
   let name_ptr_list = CArray.from_ptr name_arr_ptr count |> CArray.to_list in
   name_ptr_list |> List.map (fun ptr -> string_from_null_term_ptr !@ptr)
 
+let list_active_container_names ~(lxcpath : string) =
+  let name_arr_ptr_typ = ptr (ptr char) in
+  let name_arr_ptr_init = coerce (ptr void) name_arr_ptr_typ null in
+  let struct_arr_ptr_typ = ptr (ptr Types.lxc_container) in
+  let name_arr_ptr = allocate name_arr_ptr_typ name_arr_ptr_init in
+  let struct_arr_ptr_null = coerce (ptr void) (ptr struct_arr_ptr_typ) null in
+  let count =
+    Lxc_c.list_active_containers lxcpath name_arr_ptr struct_arr_ptr_null
+  in
+  let name_ptr_list = CArray.from_ptr name_arr_ptr count |> CArray.to_list in
+  name_ptr_list |> List.map (fun ptr -> string_from_null_term_ptr !@ptr)
+
+let list_all_container_names ~(lxcpath : string) =
+  let name_arr_ptr_typ = ptr (ptr char) in
+  let name_arr_ptr_init = coerce (ptr void) name_arr_ptr_typ null in
+  let struct_arr_ptr_typ = ptr (ptr Types.lxc_container) in
+  let name_arr_ptr = allocate name_arr_ptr_typ name_arr_ptr_init in
+  let struct_arr_ptr_null = coerce (ptr void) (ptr struct_arr_ptr_typ) null in
+  let count =
+    Lxc_c.list_all_containers lxcpath name_arr_ptr struct_arr_ptr_null
+  in
+  let name_ptr_list = CArray.from_ptr name_arr_ptr count |> CArray.to_list in
+  name_ptr_list |> List.map (fun ptr -> string_from_null_term_ptr !@ptr)
+
+                   (*$*)
 module Container = struct end
