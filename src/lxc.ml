af042a0d0ca89d5cfe85f6f799d8e8b94c37bf44
Darren Ldl <darrenldldev@gmail.com>
2019-10-21 23:41:03 +1100

    Polishing console_log related stuff

--- src/lxc.ml
+++ src/lxc.ml
@@ -11,7 +11,6 @@ type getfd_result =
   ; tty_fd : int }
 
 module Backing_store = Backing_store
-module Console_log = Console_log
 module Console_options = Console_options
 module Create_options = Create_options
 module Namespace_flags = C.Namespace_flags
@@ -478,13 +477,17 @@ module Container = struct
       |> int_to_unit_result_zero_is_ok
   end
 
-  let console_log c (options : Console_log.options) =
-    let c_struct = Console_log.c_struct_of_options options in
-    match C.console_log c.lxc_container (addr c_struct) with
-    | 0 ->
-      Ok (Console_log.result_of_c_struct (addr c_struct))
-    | _ ->
-      Error ()
+  module Console_log = struct
+    include Console_log_internal
+
+    let console_log c (options : options) =
+      let c_struct = c_struct_of_options options in
+      match C.console_log c.lxc_container (addr c_struct) with
+      | 0 ->
+        Ok (result_of_c_struct (addr c_struct))
+      | _ ->
+        Error ()
+  end
 
   module Cgroup = struct
     let get c ~key =
