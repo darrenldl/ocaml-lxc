94b360da6a3d0d7dfc0d3a5f32a12493e646c179
Darren Ldl <darrenldldev@gmail.com>
2019-10-24 17:42:09 +1100

    Added examples/create_snapshot

---
+++ examples/create_snapshot.ml
@@ -0,0 +1,11 @@
+let container_name = "ocaml-lxc_example"
+
+let () =
+  let c = Lxc.new_container container_name |> Result.get_ok in
+  match
+    Lxc.Container.Snapshot.create c
+  with
+  | Ok _ ->
+    print_endline "Ok"
+  | Error _ ->
+    print_endline "Error"
